05c5b84a52d903eb3e45f3fd70e60667be2a153b
Yutaro Mizusaki <yutaro.mizusaki.zy@fujifilm.com>
2026-09-09 07:22:50 +0000

    workshop: ym

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,18 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
@@ -39,9 +34,7 @@ customer_orders as (
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        customers.*,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
